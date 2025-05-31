28b27b8d86200778f84d4ea314a69a4b95a9d0ab
Jhonas-CLA <jjgaitan4@gmail.com>
2025-05-31 15:45:48 -0500

    Avance

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accion',
     'usuarios',
+    'productos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
